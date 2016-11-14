f6a23c9ee66b17e7885568130ee86c735a30be8c
DengSir <tdaddon@163.com>
2016-11-14 18:40:39 +0800

    enUS

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -1,14 +1,15 @@
 --[[
-zhCN.lua
+enUS.lua
 @Author  : DengSir (tdaddon@163.com)
 @Link    : https://dengsir.github.io
 ]]
 
-local L = LibStub('AceLocale-3.0'):NewLocale('tdBattlePetScript_Rematch', 'zhCN', true)
+
+local L = LibStub('AceLocale-3.0'):NewLocale('tdBattlePetScript_Rematch', 'enUS')
 if not L then return end
 
 L.TITLE = 'Rematch'
 L.NOTES = '这个脚本选择器匹配你当前Rematch插件加载的宠物队伍。'
 
-L.WRITE_SCRIPT   = '编写脚本'
+L.WRITE_SCRIPT   = 'Write script'
 L.LEVELING_FIELD = '升级栏位'
