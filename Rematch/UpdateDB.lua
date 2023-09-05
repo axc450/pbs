c1b064e704a0f638f107d4c9679628e95e291f1a
Mike L <justgiz@gmail.com>
2023-09-04 21:57:33 -0700

    Fix multiple flaws in this UpdateDB
    First: Because it was all in one loop. Maybe a non targetID team was getting attached to the wrong team.
    
    Second: as we weren't doing any moves until after the loop. GetScript was always returning false. So scripts were overriding each other.
    
    Also, as scripts were always attaching to the first team it sees, we need to look for otheres that dont have scripts.

--- Rematch/UpdateDB.lua
+++ Rematch/UpdateDB.lua
@@ -12,43 +12,62 @@ function Addon:UpdateDB()
     -- Force Rematch to update teams, so 'GetTeamIDByName' works
     Rematch.savedTeams:TeamsChanged(true)
 
-    local moveTasks = {}
-
+    -- First we need a cache list of all of our scripts.
+    -- so we can modify our scripts database without messing up the loops
+    local scriptList = {}
     for key, script in self:IterateScripts() do
+        scriptList[key] = script
+    end
+
+    -- now we first migrade scripts attached to a team with a target ID
+    for key, script in pairs(scriptList) do
         if type(key)=="number" then
 
             -- Attach script to first team found
             local teams = Rematch.savedTargets[key]
 
             if teams then
-                --print('found some teams, attaching to the first one')
-                tinsert(moveTasks, {key, teams[1], script})
+                --print('found at least one team on that target, attaching to the first one.')
+                self:RemoveScript(key)
+                self:AddScript(teams[1], script)
             else
                 print('no team found.', 'target:', Rematch.targetInfo:GetNpcName(key))
             end
-        else
+        end
+    end
 
+    -- and then migrate scripts that are attached to a team without a target
+    for key, script in pairs(scriptList) do
+        if type(key)~="number" then
             -- Look for a team with a matching name
             local teamID = Rematch.savedTeams:GetTeamIDByName(key)
 
             if teamID then
                 -- Does the team already have a matching script
                 if not self:GetScript(teamID) then
-                    -- print('found matching team with no script')
-                    tinsert(moveTasks, {key, teamID, script})
+                    -- print('found matching team with no script.')
+                    self:RemoveScript(key)
+                    self:AddScript(teamID, script)
                 else
-                    print('found a matching team, but it has a script.', key, teamID)
+                    print('found a matching team, but it has a script. looking for another …', key)
+
+                    local name = key:trim():gsub(" %(%d+%)$", "") -- take off any trailing (2)s
+                    local num = 1
+                    local newTeamID
+                    repeat
+                        num = num+1
+                        local newName = format("%s (%d)", name, num)
+                        newTeamID = Rematch.savedTeams:GetTeamIDByName(newName)
+                    until newTeamID and not self:GetScript(newTeamID)
+                    -- is there a chance that this could run forever if a team is not found?
+
+                    print('   … found a team without a script!', newTeamID, Rematch.savedTeams[newTeamID].name)
+                    self:RemoveScript(key)
+                    self:AddScript(newTeamID, script)
                 end
             else
                 print('no team found.', 'name:', key)
             end
-        end
-    end
-
-    if #moveTasks>0 then
-        for _, info in ipairs(moveTasks) do
-            self:RemoveScript(info[1])
-            self:AddScript(info[2], info[3])
         end
     end
 end
